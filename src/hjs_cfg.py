e1de6c001c3f1f5f1f050f2077c1d909b84dd99b
Lingerhk <lingerhk@gmail.com>
2016-12-09 00:06:04 -0500

    update hjs_cfg.py with local env

--- src/hjs_cfg.py
+++ src/hjs_cfg.py
@@ -17,10 +17,10 @@ if CUR_ENV ==  EnvEnum.T_DEV:
     
     BaseConf.IS_CTR_LOG     =   True
     BaseConf.LOG_LEVEL      =   1
-    BaseConf.SQL_HOST       =   "222.24.62.48"
+    BaseConf.SQL_HOST       =   "222.xx.xx.xx"
     BaseConf.SQL_PORT       =   3306
-    BaseConf.SQL_USER       =   "hjs_user"
-    BaseConf.SQL_PASSWD     =   "hjs@cms"
+    BaseConf.SQL_USER       =   "db_user"
+    BaseConf.SQL_PASSWD     =   "db_passwd"
     BaseConf.SQL_DB         =   "hjs_cms_db"
     
 
@@ -29,7 +29,15 @@ else:
     BaseConf.LOG_LEVEL      =   2
     BaseConf.SQL_HOST       =   "127.0.0.1"
     BaseConf.SQL_PORT       =   3306
-    BaseConf.SQL_USER       =   "root"
-    BaseConf.SQL_PASSWD     =   "123456"
+    BaseConf.SQL_USER       =   "db_user"
+    BaseConf.SQL_PASSWD     =   "db_passwd"
     BaseConf.SQL_DB         =   "hjs_cms_db"
+
+
+
+# with local cfg, so do not modify the file when test in the local env.
+if os.path.exists(os.path.join(os.path.dirname(__file__), 'hjs_local_cfg.py')):
+    from hjs_local_cfg import *
+
+
 
