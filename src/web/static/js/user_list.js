a5d7d7be0bd38ba30c654d4df290ed490eef4b90
Lingerhk <lingerhk@gmail.com>
2016-12-28 15:42:00 -0500

    fix bug

--- src/web/static/js/user_list.js
+++ src/web/static/js/user_list.js
@@ -1,31 +1,9 @@
 /**
- * Created by hang on 16-12-13.
+ * project: hjs_cms
+ * author: s0nnet
+ *
  */
-/*用户列表*/
 
-/*
-$(document).ready(function (){
-    $.get("/api/user/list", function(data) {
-        $("table tr:last").html("");
-        $.each(data.result, function(i, item) {
-            $("table").append(
-                "<tr><td>"+ item.uid +"</td>"+
-                "<td>"+ item.username +"</td>"+
-                "<td>"+ item.nickname +"</td>"+
-                "<td>"+ item.password +"</td>"+
-                "<td>"+ item.email +"</td>"+
-                "<td>"+ item.privilege +"</td>"+
-                "<td>"+ item.lastlogin +"</td>"+
-                "<td><div class='popup01'><a href='javascript:'>编辑</a></div> | <a href='#'>删除</a></td></tr>"
-            );
-            });
-        $(".popup01 a").click(function () {
-                popWin.showWin("500","430","用户编辑","user_edit.html");
-        });
-    });
-});
-
-*/
 
 $(document).ready(function() {
     list_user();
