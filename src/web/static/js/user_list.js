2cb6a173485278a0696a448e9d0af2460a2149ff
Lingerhk <lingerhk@gmail.com>
2016-12-28 16:08:27 -0500

    fix bug

--- src/web/static/js/user_list.js
+++ src/web/static/js/user_list.js
@@ -35,18 +35,28 @@ function list_user(){
                     var nickname = user_info["nickname"];
                     var username = user_info["username"];
                     var password = user_info["password"];
-                    var email = user_info["email"];
-                    var privilege = user_info["privilege"];
+                    var phone = user_info["phone"];
+                    var priv = user_info["priv"];
                     var lastlogin = user_info["lastlogin"];
 
+
+                    if(priv == 3){
+                        priv = "管理员";
+                    }else if(priv == 2){
+                        priv = "运营者";
+                    }else if(priv == 1){
+                        priv = "访客者";
+                    }else{
+                        priv = "--";
+                    }
 
                     $("table").append(
                         "<tr><td>"+ uid +"</td>"+
                         "<td>"+ username +"</td>"+
                         "<td>"+ nickname +"</td>"+
                         "<td>"+ password +"</td>"+
-                        "<td>"+ email +"</td>"+
-                        "<td>"+ privilege +"</td>"+
+                        "<td>"+ phone +"</td>"+
+                        "<td>"+ priv +"</td>"+
                         "<td>"+ lastlogin +"</td>"+
                         "<td><div class='popup01'><a href='#' onclick=edit_user('"+uid+"')>编辑</a></div> | <a href='#' onclick=del_user('"+ uid +"')>删除</a></td></tr>"
                     );
